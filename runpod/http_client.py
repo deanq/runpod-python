d83d933c3aebc340819ad1021801e41e28f1e0ce
Dean Quiñanola <dean.quinanola@runpod.io>
2024-07-21 21:50:31 -0700

    get_tracer using ConsoleSpanExporter

--- runpod/http_client.py
+++ runpod/http_client.py
@@ -1,11 +1,22 @@
 import aiohttp
-from opentelemetry.instrumentation.aiohttp_client import (
-    AioHttpClientInstrumentor
-)
 import requests
-from opentelemetry.instrumentation.requests import (
-    RequestsInstrumentor
+from opentelemetry.instrumentation.aiohttp_client import AioHttpClientInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor, 
+    ConsoleSpanExporter,
 )
+from opentelemetry import trace
+
+
+# Set up OpenTelemetry
+resource = Resource(attributes={"service.name": "aiohttp-client"})
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
 
 
 # Enable instrumentation
@@ -14,8 +25,20 @@ RequestsInstrumentor().instrument()
 
 
 class AsyncClientSession(aiohttp.ClientSession):
-    pass
+    def __init__(self, *args, **kwargs):
+        trace_config = AioHttpClientInstrumentor().create_trace_config()
+        super().__init__(trace_configs=[trace_config], *args, **kwargs)
+        self.tracer = trace.get_tracer(__name__)
+
+    def get_tracer(self):
+        return self.tracer
+
 
 
 class SyncClientSession(requests.Session):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.tracer = trace.get_tracer(__name__)
+
+    def get_tracer(self):
+        return self.tracer
